3b9f6e7153d723ad09656433c643a7f8190d5bb3
stump26 <moidpg@gmail.com>
2020-01-31 17:58:05 +0900

    Prepare Deploy

--- lolog-back/graphql/db.js
+++ lolog-back/graphql/db.js
@@ -1,42 +1,51 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
-import { API_KEY } from '../apikey.json';
+// import { API_KEY } from "../apikey.json";
 
+console.log("TCL: process.env.NODE_ENV", process.env.NODE_ENV);
+const API_KEY =
+  process.env.NODE_ENV === "production"
+    ? process.env.API_KEY
+    : require("../apikey.json").API_KEY;
+
+console.log("TCL: API_KEY", API_KEY);
 const RIOT_URL = "https://kr.api.riotgames.com/";
 
-export const getSummonerDTO = (summonerName)=>{
-  let REQUEST_URL = RIOT_URL + `/lol/summoner/v4/summoners/by-name/${summonerName}`;
+export const getSummonerDTO = summonerName => {
+  let REQUEST_URL =
+    RIOT_URL + `/lol/summoner/v4/summoners/by-name/${summonerName}`;
   REQUEST_URL += `?api_key=${API_KEY}`;
-  console.log('getSummonerDTO->',summonerName,'->',REQUEST_URL);
-  return fetch(REQUEST_URL)
-    .then(res=>res.json())
-}
+  console.log("getSummonerDTO->", summonerName, "->", REQUEST_URL);
+  return fetch(REQUEST_URL).then(res => res.json());
+};
 
-export const getLeagueEntryDTO = (encryptedSummonerId)=>{
-  let REQUEST_URL = RIOT_URL + `/lol/league/v4/entries/by-summoner/${encryptedSummonerId}`;
+export const getLeagueEntryDTO = encryptedSummonerId => {
+  let REQUEST_URL =
+    RIOT_URL + `/lol/league/v4/entries/by-summoner/${encryptedSummonerId}`;
   REQUEST_URL += `?api_key=${API_KEY}`;
-  console.log('getLeagueEntryDTO->',encryptedSummonerId,'->',REQUEST_URL);
-  return fetch(REQUEST_URL)
-    .then(res=>res.json())
-}
+  console.log("getLeagueEntryDTO->", encryptedSummonerId, "->", REQUEST_URL);
+  return fetch(REQUEST_URL).then(res => res.json());
+};
 
-export const getMatchlistDTO = (encryptedAccountId,from=0,to=20)=>{
-  let REQUEST_URL = RIOT_URL + `/lol/match/v4/matchlists/by-account/${encryptedAccountId}`;
-  REQUEST_URL +=`?endIndex=${to}&beginIndex=${from}&api_key=${API_KEY}`
-  console.log("TCL: getMatchlistDTO -> REQUEST_URL", REQUEST_URL)
-  return fetch(REQUEST_URL)
-    .then(res=>res.json())
-}
+export const getMatchlistDTO = (encryptedAccountId, from = 0, to = 20) => {
+  let REQUEST_URL =
+    RIOT_URL + `/lol/match/v4/matchlists/by-account/${encryptedAccountId}`;
+  REQUEST_URL += `?endIndex=${to}&beginIndex=${from}&api_key=${API_KEY}`;
+  console.log("TCL: getMatchlistDTO -> REQUEST_URL", REQUEST_URL);
+  return fetch(REQUEST_URL).then(res => res.json());
+};
 
-export const getChampiomDTO = (championId)=>{
-  let REQUEST_URL = "http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json";
-  console.log("TCL: getChampiom -> REQUEST_URL", REQUEST_URL)
+export const getChampiomDTO = championId => {
+  let REQUEST_URL =
+    "http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json";
+  console.log("TCL: getChampiom -> REQUEST_URL", REQUEST_URL);
   return fetch(REQUEST_URL)
-    .then(res=>res.json())
-    .then(json=>
-      Object.keys(json.data).reduce((acc,cur)=>{
-        acc[json.data[cur].key] = json.data[cur]
-        return acc
-      },{}))
-    .then(championDict=>championDict[championId])
-}
+    .then(res => res.json())
+    .then(json =>
+      Object.keys(json.data).reduce((acc, cur) => {
+        acc[json.data[cur].key] = json.data[cur];
+        return acc;
+      }, {})
+    )
+    .then(championDict => championDict[championId]);
+};
--- lolog-back/index.js
+++ lolog-back/index.js
@@ -1,14 +1,25 @@
 import { GraphQLServer } from "graphql-yoga";
 import resolvers from "./graphql/resolvers";
-import { API_KEY } from './apikey.json';
 
-console.log(API_KEY);
+let PLAYGROUND_FLAG;
+if (process.env.NODE_ENV === "production") {
+  PLAYGROUND_FLAG = false;
+} else if (process.env.NODE_ENV === "development") {
+  PLAYGROUND_FLAG = {
+    tabs: [
+      {
+        PLAYGROUND_ENDPOINT
+      }
+    ]
+  };
+}
 
 const server = new GraphQLServer({
-    typeDefs:"graphql/schema.graphql",
-    resolvers
-})
+  typeDefs: "graphql/schema.graphql",
+  resolvers,
+  playground: PLAYGROUND_FLAG
+});
 
-server.start(()=>{
-    console.log("Graphql Server Running at http://localhost:4000");
-})
+server.start(() => {
+  console.log("Graphql Server Running at http://localhost:4000");
+});
